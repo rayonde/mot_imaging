32d107936d84cab50d57bf90b63e703b1c9253c5
rayonde <antoineli1994@gmail.com>
2023-05-05 12:46:50 +0800

    camera update

--- controller/camera.py
+++ controller/camera.py
@@ -24,4 +24,74 @@ class CameraController:
         self.cam_list = self.system.GetCameras()
         self.num_cams = self.cam_list.GetSize()
         logging.info('Number of cameras detected: %i' % self.num_cams)
+
+        # Get camera
+        self.cam = self.cam_list.GetByIndex(0)
+
+        # Get nodemap
+        self.nodemap = self.cam.GetNodeMap()
+        self.nodemap_tldevice = self.cam.GetTLDeviceNodeMap()
+
+    
+    def close(self):
+        self.cam_list.Clear()
+        self.iface_list.Clear()
+        self.system.ReleaseInstance()
+        logging.info('Camera controller closed.')
+
+    def device_info(self):
+        """Print device info."""
         
+        node_device_information = ps.CCategoryPtr(self.nodemap_tldevice.GetNode('DeviceInformation'))
+        if ps.IsAvailable(node_device_information) and ps.IsReadable(node_device_information):
+            features = node_device_information.GetFeatures()
+            for feature in features:
+                node_feature = ps.CValuePtr(feature)
+                logging.info('%s: %s' % (node_feature.GetName(),
+                                        node_feature.ToString() if ps.IsReadable(node_feature) else 'Node not readable'))
+        else:
+            logging.info('Device control information not available.')
+    
+    def set_config(self, nodename, value):
+        """Set config node."""
+        try: 
+            node = ps.CIntegerPtr(self.nodemap.GetNode(nodename))
+            node_feature = ps.CValuePtr(node)
+            if ps.IsWritable(node_feature):
+                node_feature.SetValue(value)
+                logging.info('%s set to %s' % (node_feature.GetName(), node_feature.ToString()))
+            else:
+                logging.info('Unable to set %s to %s' % (node_feature.GetName(), node_feature.ToString()))
+        except:
+            logging.info('Unable to set %s to %s' % (nodename, value))
+
+
+    def get_config(self, nodename):
+        """Get config node."""
+        try:
+            node = ps.CIntegerPtr(self.nodemap.GetNode(nodename))
+            node_feature = ps.CValuePtr(node)
+
+            if ps.IsReadable(node_feature):
+                logging.info('%s: %s' % (node_feature.GetName(), node_feature.ToString()))
+            else:
+                logging.info('Unable to get %s' % node_feature.GetName())
+        except:
+            logging.info('Unable to get %s' % nodename)
+
+
+if __name__ == '__main__':
+    cam_controller = CameraController()
+    n = cam_controller.nodemap.GetNode('AcquisitionFrameRateEnable')
+    node = ps.CEnumerationPtr(n)
+
+    print(node.ToString())
+    print(node.GetEntryByName('On').GetValue())
+    print(node.GetEntryByName('Off').GetValue())
+    print(n)
+    print(node)
+    print(type(n))
+    print(type(node))
+    cam_controller.close()
+
+    
