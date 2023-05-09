16e3f121cac84a32ec5c187035168a96d69cbbf1
Yifan Li <antoineli1994@gmail.com>
2023-05-10 00:52:55 +0800

    Revert "camera config"
    This reverts commit 5586649866a439736ace057cc0b4cb3552d6d35f.

--- viewer/experiment.py
+++ viewer/experiment.py
@@ -8,7 +8,8 @@ class ExperimentTab(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
-        self.unit_section = "unit"
+        section = ("beam", "atoms")
+        unit_section = "unit"
         self.exp_params = {}
 
         self.left_frame = ttk.Frame(self)
@@ -16,106 +17,37 @@ class ExperimentTab(ttk.Frame):
         
         self.right_frame = ttk.Frame(self)
         self.right_frame.pack(side="right", fill="both", expand=True)
-
-        self._set_basic_frame(self.left_frame)
-        self._set_cooling_frame(self.left_frame)
         
-        self._set_repump_frame(self.right_frame)
-        
-        save = ttk.Button(self.right_frame, text="Save Experiment Config", command=self._save_config)
-        save.pack(side="bottom", fill="both", padx=5, pady=5)
-    
-    def _set_basic_frame(self, frame):
         # Basic parameters
         section_name = "atoms"
-        basic_frame = ttk.LabelFrame(frame, text="Basics")
-        basic_frame.pack(side="top", fill="both", expand=True, padx=5, pady=5)
+        basic_frame = ttk.LabelFrame(self.left_frame, text=section_name)
+        basic_frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
         
         row_idx = 0
         for key in config[section_name].keys():
             text = key.replace("_", " ").capitalize()
             ttk.Label(basic_frame, text=text).grid(row=row_idx, column=0)
-            unit = config[self.unit_section].get(key) or None
-
+            unit = config[unit_section].get(key) or None
+            print(unit)
             if unit:
                 ttk.Label(basic_frame, text=unit).grid(row=row_idx, column=2)
-                entry = FloatEntry(basic_frame )
+                entry = FloatEntry(basic_frame, state="disabled")
                 entry.grid(row=row_idx, column=1)
                 entry.insert(0, config[section_name].getfloat(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
+                self.exp_params[f"{section_name}.{key}"] = entry
             else:
-                entry = ttk.Entry(basic_frame)
+                entry = ttk.Entry(basic_frame, state="disabled")
                 entry.grid(row=row_idx, column=1)
                 entry.insert(0, config[section_name].get(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
-            row_idx += 1
-        
-    def _set_cooling_frame(self, frame):
-        # Cooling light parameters
-        section_name = "cooling"
-        label_frame = ttk.LabelFrame(frame, text="Cooling Beam")
-        label_frame.pack(side="top", fill="both", expand=True, padx=5, pady=5)
-        
-        row_idx = 0
-        for key in config[section_name].keys():
-            text = key.replace("_", " ").capitalize()
-            ttk.Label(label_frame, text=text).grid(row=row_idx, column=0)
-            
-            unit = config[self.unit_section].get(key)
-            has_unit = False if unit is None else True
-            is_float = self.is_float(config[section_name].get(key))
+                self.exp_params[f"{section_name}.{key}"] = entry
 
-            if has_unit and is_float:
-                ttk.Label(label_frame, text=unit).grid(row=row_idx, column=2)
-                entry = FloatEntry(label_frame)
-                entry.grid(row=row_idx, column=1)
-                entry.insert(0, config[section_name].getfloat(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
-            else:
-                entry = ttk.Entry(label_frame)
-                entry.grid(row=row_idx, column=1)
-                entry.insert(0, config[section_name].get(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
-            row_idx += 1
 
-    def _set_repump_frame(self, frame):
-        # Repump light parameters
-        section_name = "repump"
-        label_frame = ttk.LabelFrame(frame, text="Repump Beam")
-        label_frame.pack(side="top", fill="both", expand=True, padx=5, pady=5)
-        
-        row_idx = 0
-        for key in config[section_name].keys():
-            text = key.replace("_", " ").capitalize()
-            ttk.Label(label_frame, text=text).grid(row=row_idx, column=0)
-            
-            unit = config[self.unit_section].get(key)
-            has_unit = False if unit is None else True
-            is_float = self.is_float(config[section_name].get(key))
 
-            if has_unit and is_float:
-                ttk.Label(label_frame, text=unit).grid(row=row_idx, column=2)
-                entry = FloatEntry(label_frame)
-                entry.grid(row=row_idx, column=1)
-                entry.insert(0, config[section_name].getfloat(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
-            else:
-                entry = ttk.Entry(label_frame)
-                entry.grid(row=row_idx, column=1)
-                entry.insert(0, config[section_name].get(key))
-                self.exp_params[f"{section_name}.{key}"] = entry.get()
-            row_idx += 1
+        save = ttk.Button(self, text="Save Experiment Config", command=self._save_config)
+        save.pack(side="bottom", fill="x", padx=5, pady=5)
 
     def _save_config(self):
-        for name, value in self.exp_params.items():
+        for name, entry in self.config_params.items():
             section, key = name.split(".")
-            if value:
-                config[section][key] = str(value)
+            config[section][key] = str(entry.get())
         config.save()
-
-    def is_float(self, value):
-        try:
-            float(value)
-            return True
-        except ValueError:
-            return False
